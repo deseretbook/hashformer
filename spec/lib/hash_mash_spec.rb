74ea855de74f34fea5c5ac5b3c255f1346f18ff2
Mike Bourgeous <mike@mikebourgeous.com>
2014-06-25 16:57:11 -0600

    Added an empty test spec for HashMash.

---
+++ spec/lib/hash_mash_spec.rb
@@ -0,0 +1,10 @@
+# Hash Mash: A declarative data transformation DSL
+# Created June 2014 by Mike Bourgeous, DeseretBook.com
+# Copyright (C)2014 Deseret Book
+# See LICENSE and README.md for details.
+
+require 'hash_mash'
+
+RSpec.describe HashMash do
+  pending 'TODO: write tests'
+end
