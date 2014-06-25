a446f7d55c63ca456544d8272b94921223581121
Mike Bourgeous <mike@mikebourgeous.com>
2014-06-25 17:53:51 -0600

    Added initial tests of some of the HashMash happy paths.

--- spec/lib/hash_mash_spec.rb
+++ spec/lib/hash_mash_spec.rb
@@ -7,6 +7,35 @@ require 'hash_mash'
 
 RSpec.describe HashMash do
   describe '.validate' do
-    pending 'processes simple transformations'
+    it 'processes simple transformations' do
+      expect(HashMash.transform({a: 1}, {b: :a})).to eq({b: 1})
+      expect(HashMash.transform({a: 1, b: 2}, {b: :a, a: :b})).to eq({a: 2, b: 1})
+    end
+
+    it 'processes simple lambda transformations' do
+      expect(HashMash.transform({a: 1}, {a: ->(x){-x[:a]}})).to eq({a: -1})
+      expect(HashMash.transform({a: 'hello'}, {a: ->(x){x[:a].length}})).to eq({a: 5})
+    end
+
+    it 'processes multi-value lambda transformations' do
+      expect(HashMash.transform({a: 'hello', b: 'world'}, {c: ->(x){"#{x[:a].capitalize} #{x[:b]}"}})).to eq({c: 'Hello world'})
+      expect(HashMash.transform({a: 1, b: 2, c: 3}, {sum: ->(x){x.values.reduce(&:+)}})).to eq({sum: 6})
+    end
+
+    # TODO
+    pending 'processes lambda keys'
+    pending 'handles missing input keys'
+    pending 'handles strings as key names'
+    pending 'does not pass values not specified in transformation'
+
+    context 'input schema is given' do
+      pending 'accepts valid input hashes'
+      pending' rejects invalid input hashes'
+    end
+
+    context 'output schema is given' do
+      pending 'accepts valid output hashes'
+      pending 'rejects invalid output hashes'
+    end
   end
 end
