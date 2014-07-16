7e320abb7415180b33ed84489f56ce8e91d7369e
Mike Bourgeous <mike@mikebourgeous.com>
2014-07-15 19:48:00 -0600

    Added tests for the .map generator.

---
+++ spec/lib/hashformer/generate_spec.rb
@@ -0,0 +1,127 @@
+# Hashformer transformation generator tests
+# Created July 2014 by Mike Bourgeous, DeseretBook.com
+# Copyright (C)2014 Deseret Book
+# See LICENSE and README.md for details.
+
+require 'spec_helper'
+
+require 'hashformer'
+
+RSpec.describe Hashformer::Generate do
+  describe '.map' do
+    let(:data) {
+      {
+        first: 'Hash',
+        last: 'Former'
+      }
+    }
+
+    context 'map was given no arguments' do
+      context 'map was not given a block' do
+        it 'returns an empty array' do
+          expect(Hashformer.transform(data, { out: HF::G.map() })).to eq({out: []})
+        end
+      end
+
+      context 'map was given a block' do
+        it 'passes no arguments to the block' do
+          expect(Hashformer.transform(data, { out: HF::G.map(){|*a| a.count} })).to eq({out: 0})
+        end
+      end
+    end
+
+    context 'map was not given a block' do
+      let(:xform) {
+        {
+          name: HF::G.map(:first, :last),
+          first: HF::G.map(:first),
+          last: HF::G.map(:last)
+        }
+      }
+
+      it 'stores mapped values into an array' do
+        expect(Hashformer.transform(data, xform)).to eq({name: ['Hash', 'Former'], first: ['Hash'], last: ['Former']})
+      end
+
+      it 'adds nil to a returned array for missing keys' do
+        expect(Hashformer.transform({}, xform)).to eq({name: [nil, nil], first: [nil], last: [nil]})
+      end
+    end
+
+    context 'map was given a block that returns a string' do
+      let(:xform) {
+        {
+          name: HF::G.map(:first, :last) { |f, l| "#{f} #{l}".strip }
+        }
+      }
+
+      it 'generates the expected string for missing keys' do
+        expect(Hashformer.transform({}, xform)).to eq({name: ''})
+      end
+
+      it 'generates the expected string' do
+        expect(Hashformer.transform(data, xform)).to eq({name: 'Hash Former'})
+      end
+    end
+
+    context 'map was given a block that returns a reversed array' do
+      let(:xform) {
+        {
+          name: HF::G.map(:first, :last) { |*a| a.reverse }
+        }
+      }
+
+      it 'passes nil for missing keys' do
+        expect(Hashformer.transform({}, xform)).to eq({name: [nil, nil]})
+      end
+
+      it 'generates the expected array' do
+        expect(Hashformer.transform(data, xform)).to eq({name: ['Former', 'Hash']})
+      end
+    end
+
+    context 'map was given callables as keys and no block' do
+      let(:xform) {
+        {
+          name: HF::G.map(->(h){h[:first]}, ->(h){h[:last]})
+        }
+      }
+
+      it 'generates the expected output array' do
+        expect(Hashformer.transform(data, xform)).to eq({name: ['Hash', 'Former']})
+      end
+    end
+
+    context 'map was given paths as keys' do
+      let(:xform) {
+        {
+          name: HF::G.map(HF::G.path[:first], HF::G.path[:last]) { |*a| a.join(' ').downcase }
+        }
+      }
+
+      it 'generates the expected output' do
+        expect(Hashformer.transform(data, xform)).to eq({name: 'hash former'})
+      end
+    end
+
+    context 'map was given method chains as keys and no block' do
+      let(:xform) {
+        {
+          name: HF::G.map(HF::G.chain[:first].downcase, HF::G.chain[:last].upcase)
+        }
+      }
+
+      it 'generates the expected output' do
+        expect(Hashformer.transform(data, xform)).to eq({name: ['hash', 'FORMER']})
+      end
+    end
+  end
+
+  describe '.path' do
+    pending 'test HF::G.path'
+  end
+
+  describe '.chain' do
+    pending 'test HF::G.chain'
+  end
+end
